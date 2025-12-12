509aec30ce6b210b37fccbf6969ee865873a7e02
robson-muniz <robson_muniz@yahoo.com>
2025-12-12 16:26:17 +0000

    Limit Selector

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react';
-
-const API_URL =
-    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+import CoinCard from "./components/CoinCard.jsx";
+import LimitSelector from "./components/LimitSelector.jsx";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const App = () => {
     const [coins, setCoins] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [limit, setLimit] = useState(10)
 
     useEffect(() => {
         const getCoins = async () => {
             try {
-                const res = await fetch(API_URL);
+                const res = await fetch(`${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`, {});
 
                 if (!res.ok) {
                     throw new Error('Coin not found');
@@ -28,11 +29,24 @@ const App = () => {
         };
 
         getCoins();
-    }, []);
+    }, [limit]);
 
     return (
         <div>
             <h1>🚀 Crypto Dash</h1>
+            {loading && <p>Loading...</p>}
+            {error && <div className="error">{error}</div>}
+
+            <LimitSelector  limit={limit} onLimitChange={setLimit} />
+
+
+            {!loading && !error && (
+                <main className="grid">
+                    {coins.map((coin) => (
+                        <CoinCard key={coin.id} coin={coin} />
+                    ))}
+                </main>
+            )}
         </div>
     );
 };
